531143970bbd3464b58b06de05ad26e579de96d4
Enmanuel dlN <edelanuez.lang@gmail.com>
2020-01-16 17:14:34 +0000

    start table

--- exercises/part-1/index.js
+++ exercises/part-1/index.js
@@ -1,19 +1,33 @@
 const renderDataTable = function(data, el) {
-  const table = document.createElement('table')
-  const tableHead = document.createElement('thead')
-  for(let i = 0; i < data.length; i ++) {
-    const rowCell = document.create('tr')
+    const table = document.createElement('table')
+    const tableHead = document.createElement('thead')
+    const row = document.createElement('tr')
+    tableHead.appendChild(row)
+    table.appendChild(tableHead)
 
-    const td = document.create('td')
-    td.createTextNode
+    //add cells (th) to header row
+    for (let i = 0; i < data.length; i++) {
+        const headerData = Object.keys(data[0])[i]
+        const header = document.createElement('th')
+        header.innerText = `${headerData}`
+        row.appendChild(header)
+    }
+    el.appendChild(table)
 
-    rowCell.appendChild()
+    //
+    for (let i = 0; i < data.length; i++) {}
 
-  }
 }
 
 
 
-const animals = [{name: 'Martin', species: 'Elephant'}, {name: 'Grace', species: 'Tiger'}]
+const animals = [
+    { name: 'Martin', species: 'Elephant' },
+    { name: 'Grace', species: 'Tiger' }
+]
 const el = document.getElementById('animals')
 renderDataTable(animals, el)
+
+
+const par = document.getElementById('test')
+par.innerHTML = 'hello'

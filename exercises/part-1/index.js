18e5b9f7b0ce647282a64df8735837c3258c1dae
Enmanuel <edelanuezlang@gmail.com>
2020-01-16 11:31:33 -0500

    sync

---
+++ exercises/part-1/index.js
@@ -0,0 +1,19 @@
+const renderDataTable = function(data, el) {
+  const table = document.createElement('table')
+  const tableHead = document.createElement('thead')
+  for(let i = 0; i < data.length; i ++) {
+    const rowCell = document.create('tr')
+
+    const td = document.create('td')
+    td.createTextNode
+
+    rowCell.appendChild()
+
+  }
+}
+
+
+
+const animals = [{name: 'Martin', species: 'Elephant'}, {name: 'Grace', species: 'Tiger'}]
+const el = document.getElementById('animals')
+renderDataTable(animals, el)

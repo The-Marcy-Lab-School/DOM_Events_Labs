801bc4ab9f2574f812c0ea772a335f40debb1ef8
Enmanuel dlN <edelanuez.lang@gmail.com>
2020-01-18 03:15:50 +0000

    finish part 1, add basic styling, create target elements

--- exercises/part-1/index.js
+++ exercises/part-1/index.js
@@ -1,33 +1,67 @@
-const renderDataTable = function(data, el) {
-    const table = document.createElement('table')
-    const tableHead = document.createElement('thead')
-    const row = document.createElement('tr')
-    tableHead.appendChild(row)
-    table.appendChild(tableHead)
+function renderDataTable(collection, element) {
+    // create table
+    const table = document.createElement('table');
 
-    //add cells (th) to header row
-    for (let i = 0; i < data.length; i++) {
-        const headerData = Object.keys(data[0])[i]
-        const header = document.createElement('th')
-        header.innerText = `${headerData}`
-        row.appendChild(header)
+    // create table head
+    const tableHead = document.createElement('thead');
+
+    // create as many table headers as we need, and append to head
+    const collectionProps = Object.keys(collection[0]);
+    for (let i = 0; i < collectionProps.length; i++) {
+        const tableHeader = document.createElement('th');
+        tableHeader.innerText = `${collectionProps[i]}`;
+        tableHead.appendChild(tableHeader);
     }
-    el.appendChild(table)
 
-    //
-    for (let i = 0; i < data.length; i++) {}
+    // append table head to table
+    table.appendChild(tableHead);
 
+    // create table body
+    const tableBody = document.createElement('tbody');
+
+    // create rows, with table data, as many as in our collection
+    for (let i = 0; i < collection.length; i++) {
+        const tableRow = document.createElement('tr');
+        const keysData = Object.values(collection[i]);
+        for (let j = 0; j < keysData.length; j++) {
+            const tableData = document.createElement('td');
+            tableData.innerHTML = `${keysData[j]}`;
+            tableRow.appendChild(tableData);
+        }
+        tableBody.appendChild(tableRow);
+    }
+
+    // append table body to table
+    table.appendChild(tableBody);
+
+    //append table to el
+    element.appendChild(table);
 }
-
-
 
 const animals = [
     { name: 'Martin', species: 'Elephant' },
     { name: 'Grace', species: 'Tiger' }
-]
-const el = document.getElementById('animals')
-renderDataTable(animals, el)
+];
+
+const el = document.getElementById('animals');
+renderDataTable(animals, el);
+
+const cities = [
+    { city_name: 'New York', state: 'NY', population: 8000000 },
+    { city_name: 'San Fransisco', state: 'CA', population: 900000 }
+];
+
+const el2 = document.getElementById('cities');
+renderDataTable(cities, el2);
 
 
-const par = document.getElementById('test')
-par.innerHTML = 'hello'
+const fellows = [
+    { 'Name': 'Enmanuel', 'Age': 19, 'Tag': '@edln', 'Favorite Color': 'blue' },
+    { 'Name': 'Devonte', 'Age': 20, 'Tag': '@the_engineer', 'Favorite Color': 'purple' },
+    { 'Name': 'Laisha', 'Age': 19, 'Tag': '@newJnewMe', 'Favorite Color': 'pink' },
+    { 'Name': 'Cielo', 'Age': 19, 'Tag': '@enElCielo', 'Favorite Color': 'yellow' },
+    { 'Name': 'Paul', 'Age': 21, 'Tag': '@paully', 'Favorite Color': 'red' },
+];
+
+const el3 = document.getElementById('fellows');
+renderDataTable(fellows, el3);

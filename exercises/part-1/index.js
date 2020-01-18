0130c864c994be90f87535501f8d0cdcb73ddcb4
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-01-18 21:42:30 +0000

    eh

--- exercises/part-1/index.js
+++ exercises/part-1/index.js
@@ -1,27 +1,32 @@
 const animals = [{name: 'Martin', species: 'Elephant'}, {name: 'Grace', species: 'Tiger'}];
 
-const renderDataTable = function(array){
-  const table = document.getElementById('animals');
+const el = document.getElementById('animals');
+const renderDataTable = function(array,el){
+  const tbody = document.createElement('tbody');
   const th = document.createElement('th');
   const tr = document.createElement('tr');
   const td = document.createElement('td');
-  const nameKeys = Object.keys(animals[0]);
-  const nameValues = Object.values(animals[0])
-  // for (let i =0; i<animals.length; i++){
-  //   const name = animals[i].name[0];
-  //   const species = animals[i].species[0];
-  // }
   
-   
-  th.innerHTML += `${nameKeys}`;
-  td.innerHTML +=  `${nameValues}`;
-  table.appendChild(th);
-  th.appendChild(tr);
-  tr.appendChild(td);
-  
-  
+  for(let i = 0; i < animals.length; i++){
+    for (let name of Object.keys(animals[i])){
+      const nameValues = Object.values(animals[i]);
+      th.innerHTML += `${name}`;
+      td.innerHTML +=  `${nameValues}`;
+      el.appendChild(tbody);
+      tbody.appendChild(th);
+      th.appendChild(tr);
+      tr.appendChild(td);
+    }
+  } 
+};
+
+renderDataTable(animals,el);
+/*
+for (let i = 0; i < animals.length; i ++){
+    for (let name of Object.keys(animals[i])){
+        console.log(name)
+        let value = Object.values(animals[i])
+        console.log(value)
+    }
 }
-
-
-renderDataTable(animals);
-window.onload = renderDataTable(animals, el);
+*/

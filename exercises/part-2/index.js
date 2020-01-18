f112b06173e1ea356cb74511396b2a406fb846ab
Enmanuel dlN <edelanuez.lang@gmail.com>
2020-01-18 17:02:50 +0000

    add working solution to part2

--- exercises/part-2/index.js
+++ exercises/part-2/index.js
@@ -1,22 +1,39 @@
 class Replacer {
-    constructor(outerEl, innerEl) {
-        this.container = outerEl;
-        this.item = innerEl;
-        this.dataHold = [];
-    }
+  constructor(outerEl, innerEl) {
+    this.container = outerEl;
+    this.item = innerEl;
+    this.dataHold = '';
+  }
 
-    makeForm() {
-        //clear container
-        this.container.innerHTML = `
+  makeForm() {
+    // clear container
+    this.container.innerHTML = `
         <form action="#" id='nameForm'>
             <label for='nameInput'>Name:</label>
             <input type='text' name='newName' id='nameInput' required>
             <input type="submit" value="Set Name">
         </form>
         `;
-    }
 
-    showName() {}
+    const nameForm = document.getElementById('nameForm');
+    nameForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      // get new input
+      const newName = document.getElementById('nameInput');
+      replacer.dataHold = newName.value;
+      this.showName();
+    });
+  }
+
+  showName() {
+    this.container.innerHTML = `
+        <p id='name'>${this.dataHold}</p>
+        `;
+    const nameEl = document.getElementById('name');
+    nameEl.addEventListener('dblclick', () => {
+      replacer.makeForm();
+    });
+  }
 }
 
 const nameCont = document.getElementById('contact');
@@ -24,11 +41,5 @@ const nameEl = document.getElementById('name');
 const replacer = new Replacer(nameCont, nameEl);
 
 nameEl.addEventListener('dblclick', () => {
-    replacer.makeForm()
+  replacer.makeForm();
 });
-
-
-
-//replacer.container
-//replace name with text input and 'save' button
-//add event to save button-repalce form with new name from form

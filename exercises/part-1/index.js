46bdf9fc6cc9965c60c0bc2ef757d4b157fe54d6
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-01-19 00:12:45 +0000

    finished part 1

--- exercises/part-1/index.js
+++ exercises/part-1/index.js
@@ -1,32 +1,38 @@
 const animals = [{name: 'Martin', species: 'Elephant'}, {name: 'Grace', species: 'Tiger'}];
-
 const el = document.getElementById('animals');
+
 const renderDataTable = function(array,el){
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+
+  const nameKeys = Object.keys(array[0]);
+  for(let i = 0; i < nameKeys.length; i++){
+    const th = document.createElement('th');
+    th.innerText = `${nameKeys[i]}`;
+    thead.appendChild(th);
+  }
+
+  table.appendChild(thead);
+
   const tbody = document.createElement('tbody');
-  const th = document.createElement('th');
-  const tr = document.createElement('tr');
-  const td = document.createElement('td');
-  
-  for(let i = 0; i < animals.length; i++){
-    for (let name of Object.keys(animals[i])){
-      const nameValues = Object.values(animals[i]);
-      th.innerHTML += `${name}`;
-      td.innerHTML +=  `${nameValues}`;
-      el.appendChild(tbody);
-      tbody.appendChild(th);
-      th.appendChild(tr);
+
+  for (let i = 0; i < array.length; i++){
+    const tr = document.createElement('tr');
+    const nameValues = Object.values(array[i]);
+    for(let j = 0; j < nameValues.length; j++){
+      const td = document.createElement('td');
+      td.innerHTML = `${nameValues[j]}`;
       tr.appendChild(td);
+      }
+    tbody.appendChild(tr);
     }
-  } 
+    table.appendChild(tbody);
+    el.appendChild(table);
 };
 
+
 renderDataTable(animals,el);
-/*
-for (let i = 0; i < animals.length; i ++){
-    for (let name of Object.keys(animals[i])){
-        console.log(name)
-        let value = Object.values(animals[i])
-        console.log(value)
-    }
-}
-*/
+
+const cities = [{city_name: 'New York', state: 'NY', population: 8000000}, {city_name: 'San Fransisco', state: 'CA', population: 900000}];
+const el2 = document.getElementById('cities');
+renderDataTable(cities,el2);

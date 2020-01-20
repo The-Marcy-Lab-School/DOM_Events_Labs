08ee7dc2c5107bd8907903212445fdc2ae029f45
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-01-20 19:28:15 +0000

    fully finished

--- exercises/part-1/index.js
+++ exercises/part-1/index.js
@@ -1,14 +1,14 @@
-const animals = [{name: 'Martin', species: 'Elephant'}, {name: 'Grace', species: 'Tiger'}];
-const el = document.getElementById('animals');
+const animals = [{ name: 'Martin', species: 'Elephant' }, { name: 'Grace', species: 'Tiger' }];
+const element = document.getElementById('animals');
 
-const renderDataTable = function(array,el){
+function renderDataTable(array, el) {
   // Created table and table head
   const table = document.createElement('table');
   const thead = document.createElement('thead');
   // Grab the name keys of array at index 0
   const nameKeys = Object.keys(array[0]);
-  // Loop over the name keys and created the table header also 
-  for(let i = 0; i < nameKeys.length; i++){
+  // Loop over the name keys and created the table header also
+  for (let i = 0; i < nameKeys.length; i += 1) {
     const th = document.createElement('th');
     th.innerText = `${nameKeys[i]}`;
     thead.appendChild(th);
@@ -18,24 +18,24 @@ const renderDataTable = function(array,el){
 
   const tbody = document.createElement('tbody');
   // Loop over array and created table rows and the values of the object
-  for (let i = 0; i < array.length; i++){
+  for (let i = 0; i < array.length; i += 1) {
     const tr = document.createElement('tr');
     const nameValues = Object.values(array[i]);
-    
-    for(let j = 0; j < nameValues.length; j++){
+
+    for (let j = 0; j < nameValues.length; j += 1) {
       const td = document.createElement('td');
       td.innerHTML = `${nameValues[j]}`;
       tr.appendChild(td);
-      }
-    // Appened table rows to the table body
-    tbody.appendChild(tr); 
     }
-    table.appendChild(tbody);
-    // Appened the table to the element
-    el.appendChild(table);
-};
-renderDataTable(animals,el);
+    // Appened table rows to the table body
+    tbody.appendChild(tr);
+  }
+  table.appendChild(tbody);
+  // Appened the table to the element
+  el.appendChild(table);
+}
+renderDataTable(animals, element);
 
-const cities = [{city_name: 'New York', state: 'NY', population: 8000000}, {city_name: 'San Fransisco', state: 'CA', population: 900000}];
+const cities = [{ city_name: 'New York', state: 'NY', population: 8000000 }, { city_name: 'San Fransisco', state: 'CA', population: 900000 }];
 const el2 = document.getElementById('cities');
-renderDataTable(cities,el2);
+renderDataTable(cities, el2);
--- exercises/part-2/index.js
+++ exercises/part-2/index.js
@@ -1,26 +1,26 @@
-const name = document.getElementById('name');
-name.addEventListener('dblclick',switchInput);
+const div = document.getElementById('contact');
 
-function switchInput (event){
-  event.preventDefault();
-  let originalInput = event.target.innerHTML;
-  console.log(originalInput);
-  const form = `
+function switchInput() {
+  // switches the div inner html to a form
+  const switchForm = `
     <form id ='form'>
       <label for='new-input'>Name:</label>
-      <input type='text' name='new-name' id='new-input'
-      <input type="submit" value="${originalInput}">
+      <input type='text' name='new-name' id='new-input'> 
+      <input type="submit" value="Submit" id ="submit">
+
     <form>
   `;
-  event.target.innerHTML = form;
-  console.log(event);
-  console.log(form);
+  div.innerHTML = switchForm;
+
+  // adds an event listener on the form to change it back to a p tag with a new inout value
+  const form = document.getElementById('form');
+  const input = document.getElementById('new-input');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const p = document.createElement('p');
+    div.innerHTML = `<p>${input.value}</p>`;
+    div.appendChild(p);
+  });
 }
 
-const form = document.getElementById('form');
-form.addEventListener('submit',function (e){
-    e.preventDefault();
-    const newInput = 'hey';
-    const input = document.getElementById('new-input');
-    input.value = `${newInput}`;
-});
+div.addEventListener('dblclick', switchInput);

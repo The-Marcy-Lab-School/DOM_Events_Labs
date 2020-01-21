8511f6c82b6830a4aa9a3a3e9d8ae64b490dc7d9
EC2 Default User <ec2-user@ip-172-31-24-236.us-east-2.compute.internal>
2020-01-21 22:13:19 +0000

    updated part 1 table

--- exercises/part-1/index.js
+++ exercises/part-1/index.js
@@ -1,19 +1,25 @@
 function renderDataTable(arr, el){
     const element = document.getElementById(el);
     const thead = document.createElement('thead');
-    const row = document.createElement('tr')
+    const row = document.createElement('tr');
+    const tbody = document.createElement('tbody');
+    
+    
     // loop for table heading 
+    element.appendChild(thead);
     const keys = Object.keys(arr[1]);
     for(let i = 0; i< keys.length; i++){
-        element.appendChild(thead);
+        
         thead.appendChild(row);
         const head = document.createElement('th');
         row.appendChild(head);
         head.innerHTML = keys[i];
     }
     // nested loop for table data
+    element.appendChild(tbody);
     for(let i = 0; i < arr.length; i ++){
-        const createRow = element.insertRow(-1);
+        
+        const createRow = tbody.insertRow(-1);
         const valueArr = Object.values(arr[i]);
         
         for(let y = 0; y < keys.length; y++){
@@ -32,5 +38,38 @@ const animals = [
 const cities = [
     {city_name: 'New York', state: 'NY', population: 8000000}, 
     {city_name: 'San Fransisco', state: 'CA', population: 900000}]
+    
+const fellows = [{
+    Name: 'Enmanuel',
+    Age: 19,
+    Tag: '@edln',
+    'Favorite Color': 'blue',
+  },
+  {
+    Name: 'Devonte',
+    Age: 20,
+    Tag: '@the_engineer',
+    'Favorite Color': 'purple',
+  },
+  {
+    Name: 'Laisha',
+    Age: 19,
+    Tag: '@newJnewMe',
+    'Favorite Color': 'pink',
+  },
+  {
+    Name: 'Cielo',
+    Age: 19,
+    Tag: '@enElCielo',
+    'Favorite Color': 'yellow',
+  },
+  {
+    Name: 'Paul',
+    Age: 21,
+    Tag: '@paully',
+    'Favorite Color': 'red',
+  },
+];
+    
 
-renderDataTable(cities, 'table');
+renderDataTable(fellows, 'table');
